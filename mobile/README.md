9daf54ec290dea9cdaa22f2a936b59aae3be75e8
Jeanfranco02 <112710349+Jeanfranco02@users.noreply.github.com>
2024-06-16 21:08:25 -0600

    Inicio de Navegacion

---
+++ mobile/README.md
@@ -0,0 +1,15 @@
+COMANDOS USADOS PARA GENERAR EL PROYECTO
+
+npx create-expo-app --template
+
+COMANDOS PARA INICIALIZAR EL PROYECTO
+
+cd mobile
+cd crystalgate
+npm run android
+
+DEPENDENCIAS DEL PROYECTO
+
+npm install @react-navigation/native
+
+npm install @react-navigation/bottom-tabs
--- mobile/crystalgate/App.js
+++ mobile/crystalgate/App.js
@@ -1,12 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { CreatePermission, DocumentationRequest, Dashboard, History, Profile } from './screens/Index';
+import React from 'react';
 
+
+const Tab = createBottomTabNavigator();
+const screenOptions = {
+  tabBarShowLabel:false,
+  headerShown:false,
+  tabBarStyle:{
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 60,
+    background: "#fff"
+  }
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
+        name='CreatePermission'
+        component={CreatePermission}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return(
+              <View style={{alignItems: "center", justifyContent: "center"}}> 
+                  <image style={styles.imageStyle} source={image}/>
+              </View>
+            )
+          }
+        }}
+      />
+
+      <Tab.Screen
+      
+      />
+
+      <Tab.Screen
+      
+      />
+
+      <Tab.Screen
+      
+      />
+
+      <Tab.Screen
+      
+      />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
