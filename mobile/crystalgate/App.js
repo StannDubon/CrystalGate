b53cede51a9f5c9acff1d388d5c9a09f67540d1c
Jeanfranco02 <112710349+Jeanfranco02@users.noreply.github.com>
2024-06-26 19:11:21 -0600

    Sangria arreglada

--- mobile/crystalgate/App.js
+++ mobile/crystalgate/App.js
@@ -1,10 +1,10 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Navigation from "../crystalgate/components/navigation";
-import { useFonts } from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 //import PasswordRecovery from "../crystalgate/components/password-recovery";
 //import Verification from "../crystalgate/components/verification";
 import Loading from "./screens/loading";
@@ -15,30 +15,48 @@ import Verification from "./screens/verification";
 //EL DE PROFILE SE DEBE INGRESAR A PROFILE EL NAVIGATION YA ESTA CONFIGURADO
 //import Profile from "../crystalgate/components/profile";
 
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
     let [fontsLoaded] = useFonts({
-      'Poppins-Regular': require('./assets/fonts/Regular.ttf'),
-      'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'), // Agrega otras variantes si es necesario
+        "Poppins-Regular": require("./assets/fonts/Regular.ttf"),
+        "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"), // Agrega otras variantes si es necesario
     });
 
     if (!fontsLoaded) {
-      return <AppLoading />;
+        return <AppLoading />;
     }
 
     return (
         <NavigationContainer>
-        <Stack.Navigator >
-          <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
-          <Stack.Screen name="PasswordRecovery" component={PasswordRecovery} options={{headerShown:false}}/>
-          <Stack.Screen name="Verification" component={Verification} options={{headerShown:false}}/>
-          <Stack.Screen name="NewPassword" component={NewPassword} options={{headerShown:false}}/>
-          <Stack.Screen name="Navigation" component={Navigation} options={{headerShown:false}}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="PasswordRecovery"
+                    component={PasswordRecovery}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Verification"
+                    component={Verification}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="NewPassword"
+                    component={NewPassword}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Navigation"
+                    component={Navigation}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
